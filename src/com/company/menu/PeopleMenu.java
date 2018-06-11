236df1e105e45661df0af2029ce086ee18bcc747
James Southerland <james.f.southerland@gmail.com>
2018-06-11 12:02:45 -0400

    Added new Methods

--- src/com/company/menu/PeopleMenu.java
+++ src/com/company/menu/PeopleMenu.java
@@ -40,7 +40,7 @@ public class PeopleMenu {
                     //incorrect number
                     break;
             }
-        } catch (InputMismatchException ime){
+        } catch (InputMismatchException ime) {
             System.out.println("That is not a vailid entry. Please enter a number between 1 and 4");
             managePeople();
 
@@ -49,5 +49,104 @@ public class PeopleMenu {
 
     private void manageEmployee() {
 
+        System.out.println("You are in the Employee Menu" +
+                "\n What would you like to do:" +
+                "\n1. Create New Employee" +
+                "\n2. View All Employee" +
+                "\n3. Remove an Employee" +
+                "\n4. Edit an Employee" +
+                "\n5. Go back" +
+                "\n6. Exit Program");
+
+        try {
+
+            switch (input.nextInt()) {
+                case 1:
+                    //Create new Employee
+                    break;
+                case 2:
+                    // View all Employee
+                    break;
+                case 3:
+                    // Remove Employee
+                    break;
+                case 4:
+                    // Edit Employee
+                    break;
+                case 5:
+                    // Go Back
+                    managePeople();
+                    break;
+                case 6:
+                    //Exit Program
+                    System.out.println("Thank you for using the Zoo Program");
+                    System.exit(0);
+                    break;
+                default:
+                    //Handle if an incorrect number is put in
+                    System.out.println("That is not a vailid entry. Please enter a number between 1 and 6");
+                    manageEmployee();
+                    break;
+
+            }
+        } catch (InputMismatchException ime) {
+            input.nextLine();
+            System.out.println("That is not a vailid entry. Please enter a number between 1 and 6");
+            manageEmployee();
+
+        }
     }
+
+    private void manageVisitor() {
+
+
+        System.out.println("You are in the Visitor Menu" +
+                "\n What would you like to do:" +
+                "\n1. Create New Employee" +
+                "\n2. View All Employee" +
+                "\n3. Remove an Employee" +
+                "\n4. Edit an Employee" +
+                "\n5. Go back" +
+                "\n6. Exit Program");
+
+        try {
+
+            switch (input.nextInt()) {
+                case 1:
+                    //Create new Employee
+                    break;
+                case 2:
+                    // View all Employee
+                    break;
+                case 3:
+                    // Remove Employee
+                    break;
+                case 4:
+                    // Edit Employee
+                    break;
+                case 5:
+                    // Go Back
+                    managePeople();
+                    break;
+                case 6:
+                    //Exit Program
+                    System.out.println("Thank you for using the Zoo Program");
+                    System.exit(0);
+                    break;
+                default:
+                    //Handle if an incorrect number is put in
+                    System.out.println("That is not a vailid entry. Please enter a number between 1 and 6");
+                    manageEmployee();
+                    break;
+
+            }
+        } catch (InputMismatchException ime) {
+            input.nextLine();
+            System.out.println("That is not a vailid entry. Please enter a number between 1 and 6");
+            manageVisitor();
+
+        }
+
+    }
+
 }
